9ba2e1aaea9047fa86600ae98aa29e65f6ea016d
Balasubramaniam A <63883008+Bala-1729@users.noreply.github.com>
2021-03-13 16:47:03 +0530

    Updates

---
+++ precache-manifest.732854ea41cf57d624e89cf858ed4bc0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b836309a7de225866fad95c0c94f3c76",
+    "url": "/neuralhack-react/index.html"
+  },
+  {
+    "revision": "935f8e8e9b40906b4b65",
+    "url": "/neuralhack-react/static/css/main.bbc2d628.chunk.css"
+  },
+  {
+    "revision": "df2fd01be6cdd3835dbc",
+    "url": "/neuralhack-react/static/js/2.5c9cab98.chunk.js"
+  },
+  {
+    "revision": "89218596eac35083e13ff88abad5e4a8",
+    "url": "/neuralhack-react/static/js/2.5c9cab98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "935f8e8e9b40906b4b65",
+    "url": "/neuralhack-react/static/js/main.0138e809.chunk.js"
+  },
+  {
+    "revision": "def307fec503013f02ac",
+    "url": "/neuralhack-react/static/js/runtime-main.b86b9051.js"
+  },
+  {
+    "revision": "ecfa2d2136a3375c3f28c61bf52e07d8",
+    "url": "/neuralhack-react/static/media/Picture1.ecfa2d21.png"
+  },
+  {
+    "revision": "1e0b55ac921d70d85ee08ba3b354fdf7",
+    "url": "/neuralhack-react/static/media/akhil.1e0b55ac.png"
+  },
+  {
+    "revision": "b9dafc69a3d948861b26df6fedde6a4d",
+    "url": "/neuralhack-react/static/media/anudeep.b9dafc69.jpg"
+  },
+  {
+    "revision": "b5ce756338760947938469648a5d9924",
+    "url": "/neuralhack-react/static/media/bala.b5ce7563.png"
+  },
+  {
+    "revision": "b26bcbf15f424e97e30ee51e67b661ee",
+    "url": "/neuralhack-react/static/media/bg001.b26bcbf1.jpg"
+  },
+  {
+    "revision": "582bf9b357f5c1eb1eef69611b511036",
+    "url": "/neuralhack-react/static/media/bg003.582bf9b3.jpg"
+  },
+  {
+    "revision": "6396a95d96857242fd348434e84ac3ff",
+    "url": "/neuralhack-react/static/media/bg11.6396a95d.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/neuralhack-react/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "707b91a887c454e6db6f9a4cabe7bc69",
+    "url": "/neuralhack-react/static/media/hosp.707b91a8.jpg"
+  },
+  {
+    "revision": "e8e63bdd72dfc510d3f7778f9a029e57",
+    "url": "/neuralhack-react/static/media/rishikesh.e8e63bdd.png"
+  },
+  {
+    "revision": "7eefcb80ac2051615fc508837697630b",
+    "url": "/neuralhack-react/static/media/sandeep.7eefcb80.jpg"
+  },
+  {
+    "revision": "a8a4497ab91338e0260af82c4d35e574",
+    "url": "/neuralhack-react/static/media/title.a8a4497a.jpg"
+  },
+  {
+    "revision": "ed1a8749f2b5dfd1ac0b9cfdc7eb96cc",
+    "url": "/neuralhack-react/static/media/useridf.ed1a8749.png"
+  },
+  {
+    "revision": "54e9ec5365eeb967838ffd2a35eda50b",
+    "url": "/neuralhack-react/static/media/useridm.54e9ec53.png"
+  }
+]);
